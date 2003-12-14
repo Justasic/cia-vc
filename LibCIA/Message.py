00c62678e588e65f44aa2f35989f57b777aa62a9
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2003-12-14 07:15:21 +0000

    Scary evil functional lexical scoping abuse of doom
    git-svn-id: http://svn.navi.cx/misc/trunk/cia@1136 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

--- LibCIA/Message.py
+++ LibCIA/Message.py
@@ -27,11 +27,10 @@ from twisted.xish.xpath import XPathQuery
 import time
 
 
-class Message(object):
-    """Abstract container for a notification message. All messages
-       are represented by XML DOM trees.
-
-       A real DTD/schema is forthcoming...
+class XMLObject(object):
+    """An object based on an XML document tree. This class provides
+       methods to load it from a string or a DOM element tree, and convert
+       it back to an XML string.
 
        'xml' is either a twisted.xish.domish.Element or a string containing
        the message in XML.
@@ -42,6 +41,9 @@ class Message(object):
         else:
             self.loadFromString(xml)
 
+    def __str__(self):
+        return self.xml.toXml()
+
     def loadFromString(self, string):
         """Parse the given string as XML and set the contents of the message"""
         parser = DomishStringParser()
@@ -50,18 +52,102 @@ class Message(object):
 
     def loadFromElement(self, root):
         """Set the contents of the Message from a parsed document tree given
-           as a twisted.xish.domish.Element instance. This may add missing
-           information to the message.
+           as a twisted.xish.domish.Element instance.
            """
         self.xml = root
+        self.preprocess()
+
+    def preprocess(self):
+        """A hook where subclasses can add code to inspect a freshly
+           loaded XML document and/or fill in any missing information.
+           """
+        pass
+
+
+class XMLValidityError(Exception):
+    """This error is raised by subclasses of XMLObject that encounter problems
+       in the structure of XML documents presented to them. Normally this should
+       correspond with the document not being valid according to its schema,
+       but we don't actually use a validating parser.
+       """
+    pass
+
+
+class Message(XMLObject):
+    """Abstract container for a notification message. All messages
+       are represented by XML DOM trees. The message document type
+       is described with examples and a schema in the 'xml' directory
+       of this project.
+       """
+    def preprocess(self):
+        if self.xml.name != "message":
+            raise XMLValidityError("A Message's root node must be named 'message'")
 
         # Stamp it with the current time if it has no timestamp yet
         if not self.xml.timestamp:
             self.xml.addElement("timestamp", content="%d" % time.time())
 
 
-class Filter(object):
-    pass
+class Filter(XMLObject):
+    """A filter is a description of some subset of all valid Message objects,
+       described using a simple XML-based format and XPath. The filter document
+       type is described with examples and a schema in the 'xml' directory
+       of this project.
+
+       To test the filter against a message, call it with the message instance.
+       A boolean value will be returned.
+       """
+    def preprocess(self):
+        """Upon loading an XML document, this recursively converts the XML filter
+           to a self.__call__() function that, when called with a Message instance,
+           returns a boolean indicating whether it passes the filter.
+           """
+        self.matchFunc = self.parseFilter(self.xml)
+
+    def __call__(self, message):
+        """Test the given message against the filter defined by this class,
+           returning a boolean value.
+           """
+        return self.matchFunc(message)
+
+    def parseFilter(self, element):
+        """Given an XML element, recursively builds a python function
+           implementing the filter it describes.
+           """
+        # Pass control on to the appropriate element_* function
+        try:
+            f = getattr(self, "element_" + element.name)
+        except AttributeError:
+            raise XMLValidityError("Unknown element name in Filter: %r" % element.name)
+        return f(element)
+
+    def element_match(self, element):
+        """Evaluates to True if the text matched by our 'path' attribute matches
+           this element's content, not including leading and trailing whitespace.
+           """
+        # This works through the beautiful scary evil magic of lexical scoping.
+        # When we return a reference to f, it includes a dict with this function's
+        # scope. This conveniently gives us a way to attach the parsed xpath and the
+        # text to match.
+        xp = XPathQuery(element['path'])
+        text = str(element).strip()
+        print element.children
+        print repr(element)
+        def filterMatch(msg):
+            content = xp.queryForString(msg.xml).strip()
+            print "%r, %r" % (text, content)
+            return text == content
+        return filterMatch
+
+    def element_and(self, element):
+        """Evaluates to True if all child functions evaluate to True"""
+        childFunctions = [self.parseFilter(child) for child in element.children]
+        def filterAnd(msg):
+            for child in childFunctions:
+                if not child(msg):
+                    return False
+            return True
+        return filterAnd
 
 
 class DomishStringParser(domish.SuxElementStream):
@@ -112,9 +198,19 @@ if __name__ == "__main__":
            </body>
        </message>
        """)
+<<<<<<< .mine
+    print msg
+    f = Filter('<and><match path="/message/source/project">navi-misc</match></and>')
+    print f
+    print f(msg)
+    f = Filter('<match path="/message/source/project">navi-misc</match>')
+    print f
+    print f(msg)
+=======
     print msg.xml.toXml()
     xp = XPathQuery("/message/source/project < 10")
     print xp.queryForString(msg.xml)
+>>>>>>> .r1135
 
 
 ### The End ###
