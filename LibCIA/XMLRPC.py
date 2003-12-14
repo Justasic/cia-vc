f54db2a14ceb87a055e1212c7a94d1bf8c8e9f0a
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2003-12-14 11:22:49 +0000

    Add a little bit of XML-RPC
    git-svn-id: http://svn.navi.cx/misc/trunk/cia@1144 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

---
+++ LibCIA/XMLRPC.py
@@ -0,0 +1,66 @@
+""" LibCIA.XMLRPC
+
+A simple XMLRPC interface used for delivering messages and other
+simple unidirectional activities that should be easy to access from
+any language.
+
+A more complex interface supporting the addition of new filters
+and callbacks is available though the Perspective Broker interface
+to CIA, but that is only convenient to use from Python whereas this
+XML-RPC interface is meant to be universal.
+"""
+#
+# CIA open source notification system
+# Copyright (C) 2003 Micah Dowty <micahjd@users.sourceforge.net>
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 2.1 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
+from twisted.web import xmlrpc
+from Message import Message
+import sys
+
+
+class SimpleCIAInterface(xmlrpc.XMLRPC):
+    """A simple interface to CIA over XML-RPC.
+       Must be constructed with a reference to the current Message.Hub.
+       """
+    def __init__(self, hub):
+        self.hub = hub
+
+    def xmlrpc_deliverMessage(self, xml):
+        """Deliver the given message, provided as XML text"""
+        try:
+            self.hub.deliver(Message(xml))
+        except:
+            e = sys.exc_info()[1]
+            return xmlrpc.Fault(e.__class__.__name__, str(e))
+        return True
+
+
+if __name__ == "__main__":
+    # A simple test that just creates a hub and starts an XML-RPC server
+    from twisted.internet import reactor
+    from twisted.web import server
+    from Message import Hub
+    hub = Hub()
+    def f(msg):
+        print msg
+    hub.addClient(f)
+    r = SimpleCIAInterface(hub)
+    reactor.listenTCP(7080, server.Site(r))
+    reactor.run()
+
+### The End ###
