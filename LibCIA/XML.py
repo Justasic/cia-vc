974ba38ff73c9bbb596997e314d730d9ca9d8da5
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-01-06 07:08:07 +0000

    LGPL -> GPL
    git-svn-id: http://svn.navi.cx/misc/trunk/cia@1594 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

--- LibCIA/XML.py
+++ LibCIA/XML.py
@@ -6,18 +6,18 @@ Utilities for dealing with objects built on top of XML trees
 # CIA open source notification system
 # Copyright (C) 2003-2004 Micah Dowty <micahjd@users.sourceforge.net>
 #
-#  This library is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU Lesser General Public
-#  License as published by the Free Software Foundation; either
-#  version 2.1 of the License, or (at your option) any later version.
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
 #
-#  This library is distributed in the hope that it will be useful,
+#  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-#  Lesser General Public License for more details.
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
 #
-#  You should have received a copy of the GNU Lesser General Public
-#  License along with this library; if not, write to the Free Software
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
