6a372a4570f2a8f47732c58888772bf3866818ec
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-01-20 14:41:43 +0000

    Use an HTML <meta> tag rather than the <?xml?> processing instruction to set encoding, as some silly programs are confused by having processing instructions in an XHTML document.
    git-svn-id: http://svn.navi.cx/misc/trunk/cia@1836 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

--- LibCIA/Web/Template.py
+++ LibCIA/Web/Template.py
@@ -142,10 +142,10 @@ class Page(Nouvelle.Twisted.Page):
     document = [
         xml('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" '
             '"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">\n'),
-        xml('<?xml version="1.0" encoding="utf-8" ?>\n'),
 
         tag('html', xmlns="http://www.w3.org/1999/xhtml")[
             tag('head')[
+                xml('<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />'),
                 tag('title')[ place("pageTitle") ],
                 tag('style', type="text/css", media="all")[ "@import url(/style.css);" ],
                 ],
