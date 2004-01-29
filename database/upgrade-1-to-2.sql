0a0185b20f79b8e67d801209da17ba5ef23a3cac
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-01-29 01:13:44 +0000

    Add an upgrade script to get from db version 1 to version 2
    git-svn-id: http://svn.navi.cx/misc/trunk/cia@2121 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

---
+++ database/upgrade-1-to-2.sql
@@ -0,0 +1,34 @@
+--
+-- Upgrades a CIA database from version 1 to version 2
+--
+-- Micah Dowty
+--
+
+------ Create completely new tables
+
+CREATE TABLE IF NOT EXISTS meta
+(
+    name         VARCHAR(32),
+    value        VARCHAR(255),
+);
+
+INSERT IGNORE INTO meta VALUES( 'version', 2 );
+
+CREATE TABLE IF NOT EXISTS cache
+(
+    id     VARCHAR(32) PRIMARY KEY,
+    value  LONGBLOB NOT NULL,
+    atime  BIGINT,
+);
+
+------ Add 'NOT NULL' qualifiers
+
+ALTER TABLE stats_metadata MODIFY mime_type VARCHAR(32) NOT NULL;
+ALTER TABLE stats_metadata MODIFY value LONGBLOB NOT NULL;
+
+------ Add new columns
+
+ALTER TABLE stats_metadata ADD mtime BIGINT AFTER value;
+ALTER TABLE stats_subscriptions ADD failures INT NOT NULL DEFAULT 0 AFTER trigger;
+
+--- The End ---
