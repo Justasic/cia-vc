04feccbb6d83c0e06e3a7153bcf1f5de8a7e4fdd
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-01-30 02:29:34 +0000

    Make this valid SQL
    git-svn-id: http://svn.navi.cx/misc/trunk/cia@2192 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

--- database/init-3.sql
+++ database/init-3.sql
@@ -112,11 +112,11 @@ CREATE TABLE IF NOT EXISTS stats_relations
     target_a_path  VARCHAR(128),
     target_b_path  VARCHAR(128),
     strength       INT NOT NULL DEFAULT 0,
-    freshness      BIGINT.
+    freshness      BIGINT,
 
+    PRIMARY KEY(target_a_path, target_b_path),
     INDEX (target_a_path),
     INDEX (target_b_path),
-    PRIMARY KEY (target_a_path, target_b_path),
     FOREIGN KEY (target_a_path) REFERENCES stats_catalog(target_path) ON DELETE CASCADE,
     FOREIGN KEY (target_b_path) REFERENCES stats_catalog(target_path) ON DELETE CASCADE
 ) TYPE=INNODB;
