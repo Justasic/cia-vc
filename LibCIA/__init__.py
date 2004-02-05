31ae6c45f76ee8b1383a5bbbee7ae96140291cf5
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-02-05 22:08:43 +0000

    Version bump
    git-svn-id: http://svn.navi.cx/misc/trunk/cia@2379 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

--- LibCIA/__init__.py
+++ LibCIA/__init__.py
@@ -31,7 +31,7 @@ an eye on Subversion ;)
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
-__version__ = "pre-0.82"
+__version__ = "0.83-pre"
 
 
 # Check the python version here before we proceed further
