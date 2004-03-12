eebef64d1b4be9e4b086e93b145c37b19c2b3047
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-03-12 07:14:25 +0000

    Add a backup script that reads the same database info file CIA uses and runs mysqldump
    git-svn-id: http://svn.navi.cx/misc/trunk/cia@3085 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

---
+++ tools/backup.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+BACKUPDIR=~/backups
+
+# Name this backup according to the date
+DUMP_FILE=`date "+cia-%F.dump"`
+
+# Read database parameters from the same file CIA uses
+# The format is close enough we can run it like a shell
+# script, as long as there's no whitespace around the '='
+database=cia
+user=root
+. ~/.cia_db
+
+# The password ends up on mysqldump's command line-
+# this is bad and I don't like it, but it shouldn't
+# be a huge security problem on our machine since a
+# user can only see their own processes in /proc.
+# If your machine is different, find a more secure
+# way to do this. Some possibilities are modifying
+# mysqldump to read the password from a file, or using
+# 'expect' to send a password to mysqldump's prompt.
+mysqldump -u $user --password=$passwd $database > $BACKUPDIR/$DUMP_FILE
+bzip2 $BACKUPDIR/$DUMP_FILE
+
