58e92f8056448bdfe863b616e8ec8d5e80c0124f
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2005-01-18 16:59:45 +0000

    Fix a bug in the colorText importer that Domlette was hiding but 4DOM points out for us. We need to use importNode to change the ownership of the new colorText XML tree to match the message itself.
    git-svn-id: http://svn.navi.cx/misc/trunk/cia@6363 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

--- LibCIA/IncomingMail.py
+++ LibCIA/IncomingMail.py
@@ -152,7 +152,7 @@ class IncomingMailParser:
         # Since old-style commits didn't have any metadata, the best we can do
         # is to represent the log in a <colorText> element
         colorText = ColorTextParser().parse(self.message.get_payload()).documentElement
-        XML.bury(xml, "message", "body").appendChild(colorText)
+        XML.bury(xml, "message", "body").appendChild(xml.importNode(colorText, True))
         return xml
 
     def command_DeliverXML(self):
