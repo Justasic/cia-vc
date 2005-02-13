6a8cf372a4e0c9b84b46b1fc7197d1b125103f35
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2005-02-13 09:43:18 +0000

    Switch to a slightly less silly version scheme
    git-svn-id: http://svn.navi.cx/misc/trunk/cia@6873 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

--- LibCIA/__init__.py
+++ LibCIA/__init__.py
@@ -31,7 +31,7 @@ an eye on Subversion ;)
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
-__version__ = "pre-0.91"
+__version__ = "0.91svn"
 
 
 # Check the python version here before we proceed further
