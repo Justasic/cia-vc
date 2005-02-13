f29ec3684e4ccdfdd44bbd3d9e2d7cafd4b0519f
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2005-02-13 10:08:48 +0000

    Add an IO throttler for backups
    git-svn-id: http://svn.navi.cx/misc/trunk/cia@6874 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

--- tools/remote_backup.sh
+++ tools/remote_backup.sh
@@ -9,7 +9,8 @@ CIA_HOST=cia@flapjack
 BACKUPDIR=/navi/backups/cia
 
 DUMP_FILE=`date "+cia-%F.dump.bz2"`
+IO_LIMIT=100000
 
-ssh $CIA_HOST 'database=cia; user=root; . ~/.cia_db; nice -n 19 mysqldump -u $user --password=$passwd $database | nice -n 19 gzip' | gunzip | bzip2 > $BACKUPDIR/$DUMP_FILE
+ssh $CIA_HOST 'database=cia; user=root; . ~/.cia_db; nice -n 19 mysqldump -u $user --password=$passwd $database | ~/cia/tools/io_throttle.pl $IO_LIMIT | nice -n 19 gzip' | gunzip | bzip2 > $BACKUPDIR/$DUMP_FILE
 
 
