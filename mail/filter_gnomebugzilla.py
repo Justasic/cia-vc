afb8aeb2f4cca71bddbead0eff3cd94ae7aa1c3f
jupiter <jupiter@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2005-05-03 22:08:17 +0000

    handle reporter for both preexisting and new bug reports
    git-svn-id: http://svn.navi.cx/misc/trunk/cia@8172 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

--- mail/filter_gnomebugzilla.py
+++ mail/filter_gnomebugzilla.py
@@ -10,7 +10,6 @@ class GnomeBugzillaFilter(BugFilter):
         priority  = self.message['X-Bugzilla-Severity']
         severity  = self.message['X-Bugzilla-Priority']
         status    = self.message['X-Bugzilla-Status']
-        version   = self.message['X-Bugzilla-Version']
 
         state = 'old'
         if self.message['Subject'].split(' ')[2] == 'New:':
@@ -32,11 +31,15 @@ class GnomeBugzillaFilter(BugFilter):
             if cline.startswith("http://bugzilla.gnome.org/"):
                 self.addURL(cline)
 
-            if cline.endswith("changed:"):
-                self.readReporter(cline)
 
-    def readReporter(self, line):
-        self.addReporter(' '.join(line.split(' ')[:-1]))
+            # the format is significantly different whether this is a new bug
+            # or an old one being updated
+            if state == 'new':
+                if cline.startswith("ReportedBy:"):
+                    self.addReporter(' '.join(cline.split(' ')[1:]))
+            else:
+                if cline.endswith("changed:"):
+                    self.addReporter(' '.join(cline.split(' ')[:-1]))
 
 if __name__ == '__main__':
     GnomeBugzillaFilter().main()
