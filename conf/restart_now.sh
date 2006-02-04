dfa0fbe5659880ef19706f34eb01c30d7a20e77e
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2006-02-04 10:40:51 +0000

    More configuration changes to reflect the move off of flapjack.navi.cx
    git-svn-id: http://svn.navi.cx/misc/trunk/cia@10114 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

---
+++ conf/restart_now.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+cd /home/cia
+
+       PID=`cat cia/twistd.pid`
+       echo `date` -- Killing $PID
+       while kill -9 $PID; do sleep 0.5; done
+       echo `date` -- Killed.
+
+       echo `date` -- Restarting...
+       (cd cia; twistd2.4 -l /var/log/cia/twistd.log -oy conf/cia_navi_cx.tac)
