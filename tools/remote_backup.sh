6970fec31661bca423a8b04c225192392f1c18b7
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2006-06-22 07:12:17 +0000

    Let's commit the updated remote_backup script before the machine it was on completely dies...
    git-svn-id: http://svn.navi.cx/misc/trunk/cia@10649 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

--- tools/remote_backup.sh
+++ tools/remote_backup.sh
@@ -5,13 +5,13 @@
 # minimize the impact this has on our I/O quota.
 #
 
-CIA_HOST=cia@flapjack.navi.cx
+CIA_HOST=cia@cia.navi.cx
 BACKUPDIR=~/download/cia-backups
 
 FILE_PREFIX=`date "+cia-%F"`
 DUMP_FILE=$FILE_PREFIX.dump.gz
 TAR_FILE=$FILE_PREFIX.tar.gz
 
-ssh $CIA_HOST 'database=cia; user=root; . ~/.cia_db; nice -n 19 mysqldump -u $user --password=$passwd $database | /usr/local/bin/io-throttle 0.8 | nice -n 19 gzip' > $BACKUPDIR/$DUMP_FILE
-ssh $CIA_HOST 'cd ~/cia/data; nice -n 19 tar cf - db | /usr/local/bin/io-throttle 0.8 | nice -n 19 gzip' > $BACKUPDIR/$TAR_FILE                            
+ssh $CIA_HOST 'database=cia; user=root; . ~/.cia_db; mysqldump -u $user --password=$passwd $database | gzip' > $BACKUPDIR/$DUMP_FILE
+ssh $CIA_HOST 'cd ~/cia/data; tar cf - db | gzip' > $BACKUPDIR/$TAR_FILE                            
 
