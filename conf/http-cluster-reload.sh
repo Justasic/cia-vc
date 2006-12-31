f87aa778410c78471e437823b97248c095ab6253
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2006-12-31 06:22:36 +0000

    Adding a cheesy little script to do a graceful restart of all web servers. If Pound is doing its job, we shouldn't drop any requests.
    git-svn-id: http://svn.navi.cx/misc/trunk/cia@11087 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

---
+++ conf/http-cluster-reload.sh
@@ -0,0 +1,19 @@
+export LOGDIR=/var/log/cia
+export REQUEST_HOST=cia.navi.cx
+export REQUEST_PORT=80
+
+for port in 3930 3931 3932 3933 3934; do
+    export PORT=$port
+    pidfile=server-$PORT.pid
+
+    echo Killing $port
+    kill `cat $pidfile`
+    sleep 5
+
+    echo Starting $port
+    twistd2.4 -oy conf/official.web.tac \
+        -l $LOGDIR/server-$PORT.log --pidfile=$pidfile
+
+    echo ...
+    sleep 20
+done
