01e3a962ea8f71e5160d6bb8d0d4c64bf97f4bd5
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2007-01-05 07:01:25 +0000

    More fleshing out of templates and CSS. Most importantly, this change includes a login page.
    git-svn-id: http://svn.navi.cx/misc/trunk/cia@11106 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

--- django/cia/urls.py
+++ django/cia/urls.py
@@ -2,7 +2,14 @@ from django.conf.urls.defaults import *
 from django.conf import settings
 
 urlpatterns = patterns('',
-    (r'^account/', include('cia.accounts.urls')),
+    (r'^account/login/$', 'django.contrib.auth.views.login'),
+    (r'^account/logout/$', 'django.contrib.auth.views.logout'),
+
+    (r'^account/$', 'cia.accounts.views.index'),
+    (r'^account/projects/(?P<project_id>\d+)/$', 'cia.accounts.views.project'),
+    (r'^account/authors/(?P<project_id>\d+)/$', 'cia.accounts.views.author'),
+    (r'^account/bots/(?P<bot_id>\d+)/$', 'cia.accounts.views.bot'),
+
     (r'^admin/', include('django.contrib.admin.urls')),
 )
 
