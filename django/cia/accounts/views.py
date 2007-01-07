1677b9d01cc4c4abf838c18755bccd0ee39a0719
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2007-01-07 11:56:41 +0000

    Implemented a password recovery system.
    git-svn-id: http://svn.navi.cx/misc/trunk/cia@11119 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

--- django/cia/accounts/views.py
+++ django/cia/accounts/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render_to_response
 from django.contrib import auth
-from django.template.context import RequestContext
+from django.contrib.sessions.models import Session
+from django.template.context import RequestContext, Context
 from django.http import HttpResponseRedirect, Http404
 from django.conf import settings
 from django import newforms as forms
-from django.core.validators import ValidationError, isValidEmail
+from django.template import loader
+from django.core.mail import send_mail
 from cia.accounts import models
-import datetime
+import datetime, re
 
 
 def login_required(view_func):
@@ -38,14 +40,20 @@ def internal_login(request, username, password):
     user.last_login = datetime.datetime.now()
     user.save()
 
+def get_user(username):
+    try:
+        return auth.models.User.objects.get(username=username)
+    except auth.models.User.DoesNotExist:
+        return None
+
 def login(request, next_page, template_name="accounts/login.html"):
     """Simple login form view which doesn't rely on Django's current
        inflexible oldforms-based auth view.
        """
     if request.POST:
         error = internal_login(request,
-                               request.POST['username'],
-                               request.POST['password'])
+                               request.POST.get('username'),
+                               request.POST.get('password'))
         if not error:
             return HttpResponseRedirect(next_page)
     else:
@@ -57,6 +65,105 @@ def login(request, next_page, template_name="accounts/login.html"):
         )))
 
 
+def send_mail_to_user(user, template_name, **context_dict):
+    """Send a single email message to a registered user. This formats their
+       email address using their full name, and automatically treats the
+       first non-blank line of the rendered template as a message subject.
+
+       The template context will automatically include 'user'.
+       """
+    context = Context(context_dict)
+    context['user'] = user
+    subject, message = loader.render_to_string(template_name, context).lstrip().split("\n", 1)
+    subject = subject.strip()
+    message = message.strip()
+
+    # Sanitize the user's name for inclusion in an RFC822 header
+    full_name = re.sub("[!<>@:;\\\\'\"\[\]\r\n\t]", "", user.get_full_name())
+    send_mail(subject, message, None, ["%s <%s>" % (full_name, user.email)])
+
+def lost(request, next_page, recovery_page):
+    error = None
+    if request.POST:
+        user = get_user(request.POST.get('username'))
+        if not user:
+            error = "Incorrect username."
+        if not error:
+            # Password recovery works via a special kind of session
+            # which is transmitted over e-mail, rather than via a
+            # cookie. Create a session which includes the username
+            # this request was generated for.
+
+            key = Session.objects.get_new_session_key()
+            expire_date = datetime.datetime.now() + datetime.timedelta(hours=12)
+            Session.objects.save(key, {
+                'account_recovery_session': True,
+                'username': user.username,
+                }, expire_date)
+            
+            send_mail_to_user(user, "accounts/recovery_mail.txt",
+                              request = request,
+                              recovery_path = recovery_page % key)
+            return render_to_response('accounts/recovery_mail_sent.html')
+    return render_to_response('accounts/recovery_form.html', RequestContext(request, {'error': error}))
+
+def get_recovery_session(key):
+    """Get an account recovery session, with extra checks to make sure
+       it's the right type of session and it hasn't expired.
+       """
+    try:
+        session = Session.objects.get(session_key=key)
+    except Session.DoesNotExist:
+        return None
+
+    if session.expire_date < datetime.datetime.now():
+        return None
+
+    decoded = session.get_decoded()
+    if not decoded.get('account_recovery_session'):
+        return None
+    return decoded
+
+class ResetPasswordForm(forms.Form):
+    password = forms.CharField(min_length=5, max_length=30)
+    password2 = forms.CharField()
+
+def reset(request, key, next_page):
+    session = get_recovery_session(key)
+    if not session:
+        return render_to_response('accounts/recovery_key_error.html', RequestContext(request))
+    user = get_user(session.get('username'))
+
+    if request.POST:
+        form = ResetPasswordForm(request.POST)
+        form.full_clean()
+        validate_password_confirmation(form)
+        validate_test_cookie(form, request)
+
+        if not form.errors:
+            user.set_password(form.data['password'])
+            user.save()
+
+            # Try to log in using the new password
+            loginError = internal_login(request, user.username, form.data['password'])
+            if loginError:
+                # This might happen if the account is deactivated.
+                form.errors['submit'] = forms.util.ErrorList([loginError])
+            else:
+                # We're in successfully. Expire the recovery session.
+                Session.objects.save(key, None, datetime.datetime.now())
+                return HttpResponseRedirect(next_page)
+    else:
+        form = None
+
+    request.session.set_test_cookie()
+    return render_to_response('accounts/reset_password.html', RequestContext(request, {
+        'form_path': request.path,
+        'username': user.username,
+        'form': form,
+        }))
+
+
 class RegistrationForm(forms.Form):
     first_name = forms.CharField(max_length=30)
     last_name = forms.CharField(max_length=30)
@@ -65,12 +172,6 @@ class RegistrationForm(forms.Form):
                                 error_message='Only A-Z, 0-9, "_", "-", and "." allowed.')
     password = forms.CharField(min_length=5, max_length=30)
     password2 = forms.CharField()
-
-def get_user(username):
-    try:
-        return auth.models.User.objects.get(username=username)
-    except auth.models.User.DoesNotExist:
-        return None
 
 def validate_old_password(form, user, field_name='password'):
     if not form.errors.get(field_name) and not user.check_password(form.data.get(field_name)):
@@ -81,14 +182,16 @@ def validate_password_confirmation(form, field_name='password'):
     if not form.errors.get(field2_name) and form.data.get(field_name) != form.data.get(field2_name):
         form.errors[field2_name] = forms.util.ErrorList(["Your passwords do not match."])
 
+def validate_test_cookie(form, request):
+    if not request.session.test_cookie_worked():
+        form.errors['submit'] = forms.util.ErrorList(["Cookies must be enabled."])
+
 def register(request, next_page, template_name="accounts/register.html"):
     if request.POST:
         form = RegistrationForm(request.POST)
         form.full_clean()
         validate_password_confirmation(form)
-
-        if not request.session.test_cookie_worked():
-            form.errors['submit'] = forms.util.ErrorList(["Cookies must be enabled."])
+        validate_test_cookie(form, request)
 
         if not form.errors:
             # The username still might be taken, but let's test for that atomically
@@ -120,7 +223,7 @@ def register(request, next_page, template_name="accounts/register.html"):
     else:
         form = None
 
-    request.session.set_test_cookie()
+    request.session.delete_test_cookie()
     return render_to_response(template_name, RequestContext(request, {'form': form}))
 
 
