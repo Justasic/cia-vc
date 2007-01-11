1330fb2649eb56fc6a764db3ac100e7a222afcc0
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2007-01-11 08:08:48 +0000

    Split the 'accounts' app's views into assets, authplus, and bots. Authplus could almost be a separate app, but the profile editing page introduces a little bit of coupling between authplus and the asset navigation module.
    git-svn-id: http://svn.navi.cx/misc/trunk/cia@11144 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

---
+++ apps/accounts/assets.py
@@ -0,0 +1,131 @@
+from cia.apps.accounts import models, authplus
+from django.http import Http404
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
+
+
+###########################
+#    Asset Navigation     #
+###########################
+
+def get_default_asset_id(request, asset_type):
+    """Return the default asset ID for a particular type. This will
+       try, in order:
+
+          1. The default recorded in the current session
+          2. The first object returned by all_for_user
+          3. 'add', to direct users to an asset creation page.
+       """
+    model = get_asset_by_type(asset_type)
+    all = model.objects.all_for_user(request.user)
+
+    default_id = request.session.get('default_' + asset_type)
+    if default_id:
+        try:
+            all.get(pk=default_id)
+            return default_id
+        except models.UserAsset.DoesNotExist:
+            pass
+
+    try:
+        return all[0].id
+    except IndexError:
+        pass
+
+    return 'add'
+
+def get_user_asset_types(request, current=None):
+    """Return a list which summarizes a user's assets, for navigation."""
+    return [{
+        'asset_type': model._meta.asset_type,
+        'is_current': model._meta.asset_type == current,
+        'verbose_name_plural': model._meta.verbose_name_plural,
+        'count': model.objects.all_for_user(request.user).count,
+        'default_id': get_default_asset_id(request, model._meta.asset_type),
+        }
+        for model in models.AssetManager.models]
+
+def get_asset_by_type(asset_type):
+    for model in models.AssetManager.models:
+        if model._meta.asset_type == asset_type:
+            return model
+    raise Http404
+
+def get_asset_add_context(request, asset_type):
+    """Returns a context for adding any asset. This raises
+       a 404 error if the asset_type is not valid. This includes
+       all context variables required for navigation.
+       """
+    model = get_asset_by_type(asset_type)
+    return RequestContext(request, {
+        'asset_types': get_user_asset_types(request, asset_type),
+        'asset_type': asset_type,
+        'asset_type_name': model._meta.verbose_name,
+        'user_assets': model.objects.all_for_user(request.user),
+        'add': True,
+        'form_path': request.path,
+        })
+
+def get_asset_edit_context(request, asset_type, asset_id):
+    """Returns a context for editing any asset. This raises a 404
+       error if the asset_type is not valid or if the asset_id does
+       not refer to a UserAsset for the current user. The result
+       includes all context variables required for navigation.
+       """
+    model = get_asset_by_type(asset_type)
+    asset_id = int(asset_id)
+    user_asset = model.objects.all_for_user(request.user).get(pk=asset_id)
+
+    # Set as the default asset for this type
+    request.session['default_' + asset_type] = asset_id
+
+    return RequestContext(request, {
+        'asset_types': get_user_asset_types(request, asset_type),
+        'asset_type': asset_type,
+        'asset_type_name': model._meta.verbose_name,
+        'user_assets': model.objects.all_for_user(request.user),
+        'asset_id': asset_id,
+        'user_asset': user_asset,
+        'form_path': request.path,
+        })
+
+
+###########################
+#     Profile Editing     #
+###########################
+
+@login_required
+def profile(request):
+    if 'change-password' in request.POST:
+        password_form = authplus.do_change_password(request)
+    else:
+        password_form = None
+
+    if 'change-profile' in request.POST:
+        profile_form = authplus.do_change_profile(request)
+    else:
+        profile_form = authplus.ChangeProfileForm(request.user)
+
+    return render_to_response('accounts/profile.html', RequestContext(request, {
+        'profile': True,
+        'asset_types': get_user_asset_types(request),
+        'password_form': password_form,
+        'profile_form': profile_form,
+        }))
+
+
+###########################
+#      Stats Assets       #
+###########################
+
+@login_required
+def stats_asset(request, asset_type, asset_id):
+    """Generic form for editing stats-based assets"""
+    ctx = get_asset_edit_context(request, asset_type, asset_id)
+    return render_to_response('accounts/stats_asset.html', ctx)
+
+@login_required
+def add_stats_asset(request, asset_type):
+    """Generic form for adding stats-based assets"""
+    ctx = get_asset_add_context(request, asset_type)
+    return render_to_response('accounts/add_stats_asset.html', ctx)
---
+++ apps/accounts/bots.py
@@ -0,0 +1,120 @@
+from cia.apps.accounts import models, assets
+from django import newforms as forms
+from django.contrib.contenttypes.models import ContentType
+from django.http import HttpResponseRedirect, Http404
+from django.shortcuts import render_to_response
+
+
+###########################
+#        Add Bot          #
+###########################
+
+class AddBotForm(forms.Form):
+    network = forms.CharField()
+    channel = forms.RegexField(r"^[^\s\x00-\x1f,%]+$", max_length=63,
+                               error_message='Must be a valid IRC channel name.')
+
+class AddNetworkForm(forms.Form):
+    netname = forms.CharField(max_length=200)
+    server = forms.RegexField(r"^([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,6}(:\d+)?$", max_length=120,
+                               error_message='Must be a valid hostname with optional port.')
+
+class MultiForm:
+    """A simple wrapper for conditionally validating multiple forms
+       with the same data and error dictionaries.
+       """
+    def __init__(self, bindTo):
+        self.bindTo = bindTo
+        self.data = {}
+        self.clean_data = {}
+        self.errors = {}
+
+    def validate(self, form):
+        inst = form(self.bindTo)
+        inst.full_clean()
+        self.errors.update(inst.errors)
+        self.data.update(inst.data.items())
+        if inst.clean_data:
+            self.clean_data.update(inst.clean_data.items())
+
+def validate_network(form, field_name='network'):
+    try:
+        return models.Network.objects.get(pk=int(form.data.get('network')))
+    except (ValueError, models.Network.DoesNotExist):
+        form.errors['network'] = forms.util.ErrorList(["Select a network."])
+
+@login_required
+def add_bot(request, asset_type):
+    form = MultiForm(request.POST)
+
+    if request.POST:
+        form.validate(AddBotForm)
+        if form.clean_data.get('network') == '_other':
+            form.validate(AddNetworkForm)
+            network = None
+        else:
+            network = validate_network(form)
+
+        if not form.errors:
+            if not network:
+                # No network was explicitly mentioned in the
+                # form- the user picked "Other". We'll look
+                # up an existing network using the supplied
+                # hostname, creating a new one if necessary.
+
+                uri = "irc://%s/" % form.clean_data['server']
+                network = models.Network.objects.get_or_create(
+                    uri__iexact = uri,
+                    defaults = dict(uri = uri,
+                                    description = form.clean_data['netname'],
+                                    created_by = request.user),
+                    )[0]
+
+            # Add the "#" in front of the channel, if necessary
+            channel = form.clean_data['channel']
+            if channel[0] not in '#&':
+                channel = '#' + channel
+
+            # Now look up a matching bot. We might have to create this too.
+            bot = models.Bot.objects.get_or_create(
+                network = network,
+                location__iexact = channel,
+                defaults = dict(location=channel),
+                )[0]
+
+            # Finally, create a new UserAsset.
+            user_asset = models.UserAsset.objects.get_or_create(
+                user = request.user,
+                content_type = ContentType.objects.get_for_model(models.Bot),
+                object_id = bot.id,
+                )[0]
+
+            return HttpResponseRedirect("/account/%s/%s/" % (
+                user_asset.asset._meta.asset_type, user_asset.id,
+                ))
+
+    ctx = assets.get_asset_add_context(request, asset_type)
+    ctx.update({
+        'networks': models.Network.objects.all(),
+        'form': form,
+        })
+    return render_to_response('accounts/add_bot.html', ctx)
+
+
+###########################
+#        Edit Bot         #
+###########################
+
+class EditBotForm(forms.Form):
+    is_active = forms.BooleanField()
+
+@login_required
+def bot(request, asset_type, asset_id):
+    ctx = assets.get_asset_edit_context(request, asset_type, asset_id)
+    user_asset = ctx['user_asset']
+    bot = user_asset.asset
+
+    form = EditBotForm(request.POST or bot.__dict__)
+    ctx['form'] = form
+
+    return render_to_response('accounts/bot.html', ctx)
