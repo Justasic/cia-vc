6b7642fd4cc75971e83ba8196465bc525c045fa9
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2007-01-21 03:18:53 +0000

    Finally tricked CSS into doing what I want
    git-svn-id: http://svn.navi.cx/misc/trunk/cia@11238 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

--- apps/doc/views.py
+++ apps/doc/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
-from docutils.core import publish_parts
+from django.http import Http404
 from django.conf import settings
+from docutils.core import publish_parts
 import os
 
 def page(request, path):
-    f = open(os.path.join(settings.CIA_DOC_PATH, path))
+    try:
+        f = open(os.path.join(settings.CIA_DOC_PATH, path))
+    except IOError:
+        raise Http404
+
     return render_to_response('layout_doc.html', RequestContext(request, {
         'parts': publish_parts(
             source = f.read(),
