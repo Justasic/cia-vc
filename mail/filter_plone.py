6f82d9996d7a18e1d0ce64d07ebbcdebf9dcae2e
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2007-02-23 18:47:51 +0000

    The Plone mailing list filter is also obsolete now. The only project which still relies on filterlib is XFree86.
    git-svn-id: http://svn.navi.cx/misc/trunk/cia@11419 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

--- mail/filter_plone.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-from filter_python import PythonFilter
-
-# As far as I can tell, plone and python are using the same commit format
-class PloneFilter(PythonFilter):
-    project = 'plone'
-
-if __name__ == '__main__':
-    PloneFilter().main()
