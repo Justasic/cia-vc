f4f75feadbf73b642376aa4d8ff69ee0ac15d6bd
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2007-02-26 10:10:49 +0000

    Put the new Django-based stats site online. It's still missing many features, but this should make it easier to do browser compatibility testing and to collect community feedback. To test it, replace /stats with /stats-experimental. For example: http://cia.navi.cx/stats-experimental/project/navi-misc/
    git-svn-id: http://svn.navi.cx/misc/trunk/cia@11434 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

--- apps/urls.py
+++ apps/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
 #    (r'^admin/', include('django.contrib.admin.urls')),
     (r'^doc/', include('cia.apps.doc.urls')),
     (r'^images/', include('cia.apps.images.urls')),
-    (r'^stats/', include('cia.apps.stats.urls')),
+    (r'^stats-experimental/', include('cia.apps.stats.urls')),
     (r'^feedback/', 'cia.apps.feedback.feedback'),
 )
 
