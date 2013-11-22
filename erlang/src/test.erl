3bc20739d4db8c8189bf6dc0c046dbb30ffe70b9
Justin Crawford <Justasic@gmail.com>
2013-11-22 00:41:29 -0800

    Bye Junk.

--- erlang/src/test.erl
+++
@@ -1,7 +0,0 @@
--module(test).
--export([main/0]).
-
-main() ->
-    catch(cbot_logger:start()),
-    {ok, Sup} = cbot_irc_supervisor:start_link({tcp, "localhost", 6667}, [ {nick, "erlbot"} ]),
-    register(cbot_sup, Sup).
