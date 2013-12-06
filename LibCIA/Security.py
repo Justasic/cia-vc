a9a82de5bacd65a630c70bfd150599af724a12a8
Justin Crawford <Justasic@gmail.com>
2013-12-05 20:50:46 -0800

    My fixes used hashlib wrong, fixed that.

--- LibCIA/Security.py
+++ LibCIA/Security.py
@@ -400,10 +400,11 @@ class User:
         uid = self._getUid(cursor)
         for capability in capabilities:
             rep = repr(capability)
+            print rep
             cursor.execute("INSERT IGNORE INTO capabilities (uid, cap_md5, cap_repr)"
                            " VALUES(%d, %s, %s)" % (
                 uid,
-                Database.quote(hashlib.new(rep).hexdigest(), 'char'),
+                Database.quote(hashlib.md5(rep).hexdigest(), 'char'),
                 Database.quote(rep, 'text')))
 
     def require(self, *capabilities):
@@ -430,7 +431,7 @@ class User:
         if capabilities:
             return "SELECT 1 FROM capabilities WHERE uid = %d AND (%s) LIMIT 1" % (
                 uid,
-                " OR ".join(["cap_md5 = " + Database.quote(hashlib.new(repr(c)).hexdigest(),
+                " OR ".join(["cap_md5 = " + Database.quote(hashlib.md5(repr(c)).hexdigest(),
                                                            'char') for c in capabilities]),
                 )
         else:
